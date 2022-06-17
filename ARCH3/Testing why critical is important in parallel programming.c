96a3cc9d514ab1cbc9362ab5a238f4bac2ac86e8
Bilal.b <74218805+Bilal-Belli@users.noreply.github.com>
2022-06-17 17:43:11 +0100

    Create Testing why critical is important in parallel programming.c

---
+++ ARCH3/Testing why critical is important in parallel programming.c
@@ -0,0 +1,31 @@
+#include <omp.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int main() {
+  const int N = 100;
+  int a[N];
+  for (int i = 0; i < N; i++)
+    a[i] = i;
+  int local_sum, sum;
+  #pragma omp parallel private(local_sum) {
+    local_sum = 0;
+
+    //the array is distributde statically between threads
+    #pragma omp
+    for schedule(dynamic)
+    for (int i = 0; i < N; i++) {
+      local_sum += a[i];
+    }
+
+    //each thread calculated its local_sum. ALl threads have to add to
+    //the global sum. It is critical that this operation is atomic.
+    printf("local_sum=%d theread number=%d\n", local_sum, omp_get_thread_num());
+    //#pragma omp critical
+    #pragma omp barrier
+    sum += local_sum;
+  }
+  printf("sum=%d should be %d\n", sum, N * (N - 1) / 2);
+}
