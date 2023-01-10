32ba4be8f146064e78c6b03966b5e8849ccd0778
Bilal.b <74218805+Bilal-Belli@users.noreply.github.com>
2023-01-10 19:11:45 +0100

    Create MPI_ANNEAU_EXECUTION.c

---
+++ HPC/MPI_ANNEAU_EXECUTION.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <mpi.h>
+
+int main () {
+	int rank, value, size;
+	MPI_Status status;
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+	MPI_Comm_size( MPI_COMM_WORLD, &size );
+	// do { //pour répéter l'entrie des données
+		if (rank == 0) {
+			scanf( "%d", &value );
+			MPI_Send( &value, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD );
+		}
+		else {
+			MPI_Recv( &value, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD,&status );
+			if (rank < size - 1){
+				MPI_Send( &value, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD );
+			}
+		}
+		printf( "Process %d got %d\n", rank, value );
+ 	// } while (value >= 0);
+ 	
+ 	MPI_Finalize( );
+ 	return 0;
+}
